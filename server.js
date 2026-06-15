feacd753e82dec36485ca7f5fc049432c25381e5
MiihGit <michelle.silva270209@gmail.com>
2026-06-15 20:28:43 -0300

    Fix: remove submódulos inválidos, serve arquivos estáticos via Express

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 // ============================================================================
 // CONFIGURAÇÃO DO GMAIL
