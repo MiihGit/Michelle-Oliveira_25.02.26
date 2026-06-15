4078057148063e3abf60f90143d9fb6744b58d89
MiihGit <michelle.silva270209@gmail.com>
2026-06-15 20:32:53 -0300

    Fix: Vercel serve arquivos estáticos diretamente (index.html, sw.js, manifest.json)

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(__dirname));
 
 // ============================================================================
 // CONFIGURAÇÃO DO GMAIL
